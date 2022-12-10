f2e8fb944eb624df3bf8272b4952380dfd69027c
Christopher Whitley <aristurtledev@gmail.com>
2022-12-09 23:04:36 -0500

    Testing doc gen

---
+++ tools/DocGen/DocGenApp.cs
@@ -0,0 +1,18 @@
+﻿using XmlDocMarkdown.Core;
+
+args = new string[]
+{
+    "AsepriteDotNet",
+    "../../.docs",
+    "--source", "https://github.com/AristurtleDev/AsepriteDotNet/tree/main/source/AsepriteDotNet",
+    "--namespace", "AsepriteDotNet",
+    "--visibility", "public",
+    "--obsolete",
+    "--skip-unbrowsable",
+    "--clean",
+    "--permalink", "pretty",
+    "--namespace-pages",
+    "--newline", "auto"
+};
+
+return XmlDocMarkdownApp.Run(args);

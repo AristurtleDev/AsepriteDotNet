d38704f70bb1f3d5d0fe50eb5438a545760e7bb3
Christopher Whitley <aristurtledev@gmail.com>
2024-02-17 23:23:15 -0500

    Use frozen set for collection property

--- source/AsepriteDotNet/AsepriteImageCel.cs
+++ source/AsepriteDotNet/AsepriteImageCel.cs
@@ -2,6 +2,8 @@
 //  Licensed under the MIT license.
 //  See LICENSE file in the project root for full license information.
 
+using System.Collections.Frozen;
+
 namespace AsepriteDotNet;
 
 /// <summary>
@@ -20,17 +22,17 @@ public sealed class AsepriteImageCel : AsepriteCel
     public int Height { get; }
 
     /// <summary>
-    /// An array of <see cref="Rgba32"/> color elements that represents the pixel data that makes up the image for this
-    /// cel. Order of the color elements starts with the top-left most pixel and is read left-to-right from
-    /// top-to-bottom.
+    /// A collection of the <see cref="Rgba32"/> color elements that represents the pixel data that makes up the image
+    /// for this <see cref="AsepriteCel"/>.  Order of the color elements starts with the top-left most pixel and is read
+    /// left-to-right from top-to-bottom.
     /// </summary>
-    public Rgba32[] Pixels { get; }
+    public FrozenSet<Rgba32> Pixels { get; }
 
     internal AsepriteImageCel(AsepriteLayer layer, int x, int y, int opacity, int width, int height, Rgba32[] pixels)
         : base(layer, x, y, opacity)
     {
         Width = width;
         Height = height;
-        Pixels = pixels;
+        Pixels = pixels.ToFrozenSet();
     }
 }

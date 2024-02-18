7db8804f9ded3e223ee2271c43f282315b1ff75c
Christopher Whitley <aristurtledev@gmail.com>
2024-02-17 22:50:47 -0500

    Convert fields to properties

--- source/AsepriteDotNet/AsepriteCel.cs
+++ source/AsepriteDotNet/AsepriteCel.cs
@@ -12,28 +12,37 @@ public abstract class AsepriteCel
     /// <summary>
     /// The <see cref="AsepriteLayer"/> that this <see cref="AsepriteCel"/> exists on.
     /// </summary>
-    public AsepriteLayer Layer;
+    public AsepriteLayer Layer { get; }
 
     /// <summary>
     /// The x-coordinate position of this <see cref="AsepriteCel"/>, relative to the bounds of the
     /// <see cref="AsperiteFrame"/> it is in.
     /// </summary>
-    public int X;
+    public int X { get; }
 
     /// <summary>
     /// The y-coordinate position of this <see cref="AsepriteCel"/>, relative to the bounds of the
     /// <see cref="AsepriteFrame"/> it is in.
     /// </summary>
-    public int Y;
+    public int Y { get; }
 
     /// <summary>
     /// The opacity level of this <see cref="AsepriteCel"/>.
     /// </summary>
-    public int Opacity;
+    public int Opacity { get; }
 
     /// <summary>
     /// The <see cref="AsepriteUserData"/> that was set in the properties for this <see cref="AsepriteCel"/> in
     /// Aseprite.
     /// </summary>
-    public AsepriteUserData UserData;
+    public AsepriteUserData UserData { get; }
+
+    internal AsepriteCel(AsepriteLayer layer, int x, int y, int opacity)
+    {
+        Layer = layer;
+        X = x;
+        Y = y;
+        Opacity = opacity;
+        UserData = new AsepriteUserData();
+    }
 }

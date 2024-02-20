96c0c0c8c7a305946f9ef955e5cff7b7089751d7
Christopher Whitley <aristurtledev@gmail.com>
2024-02-20 00:14:27 -0500

    Added CelProperties

---
+++ source/AsepriteDotNet/InternalStructs/CelProperties.cs
@@ -0,0 +1,41 @@
+//  Copyright (c) Christopher Whitley. All rights reserved.
+//  Licensed under the MIT license.
+//  See LICENSE file in the project root for full license information
+
+using System.Runtime.InteropServices;
+
+namespace AsepriteDotNet;
+
+[StructLayout(LayoutKind.Explicit)]
+internal struct CelProperties
+{
+    public const int SizeOf = sizeof(ushort) +  //  LayerIndex
+                              sizeof(short) +   //  X
+                              sizeof(short) +   //  Y
+                              sizeof(byte) +    //  Opacity
+                              sizeof(ushort) +  //  Type
+                              sizeof(short) +   //  ZIndex
+                              sizeof(byte) * 5; //  Ignore1
+
+
+    [FieldOffset(0)]
+    public ushort LayerIndex;
+
+    [FieldOffset(sizeof(ushort))]
+    public short X;
+
+    [FieldOffset(sizeof(short))]
+    public short Y;
+
+    [FieldOffset(sizeof(short))]
+    public byte Opacity;
+
+    [FieldOffset(sizeof(byte))]
+    public ushort Type;
+
+    [FieldOffset(sizeof(ushort))]
+    public short ZIndex;
+
+    [FieldOffset(sizeof(short))]
+    public byte[] Ignore1;
+}

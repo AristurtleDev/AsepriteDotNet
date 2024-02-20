c4764228afc3ebc19c2daab56f65f0ad9ee24ab5
Christopher Whitley <aristurtledev@gmail.com>
2024-02-19 22:36:58 -0500

    Add LayerChunkHeader

---
+++ source/AsepriteDotNet/InternalStructs/LayerChunkHeader.cs
@@ -0,0 +1,49 @@
+//  Copyright (c) Christopher Whitley. All rights reserved.
+//  Licensed under the MIT license.
+//  See LICENSE file in the project root for full license information
+
+using System.Runtime.InteropServices;
+
+namespace AsepriteDotNet;
+
+[StructLayout(LayoutKind.Explicit)]
+internal struct LayerChunkHeader
+{
+    public const int SizeOf = sizeof(ushort) +      //  Flags
+                              sizeof(ushort) +      //  Type
+                              sizeof(ushort) +      //  Level
+                              sizeof(ushort) +      //   DefaultWidth
+                              sizeof(ushort) +      //  DefaultHeight
+                              sizeof(ushort) +      //  BlendMode
+                              sizeof(byte) +        //  Opacity
+                              (sizeof(byte) * 3) +  //  Ignore
+                              sizeof(ushort);       //  NameLen
+
+
+    [FieldOffset(0)]
+    public ushort Flags;
+
+    [FieldOffset(sizeof(ushort))]
+    public ushort Type;
+
+    [FieldOffset(sizeof(ushort))]
+    public ushort Level;
+
+    [FieldOffset(sizeof(ushort))]
+    public ushort DefaultWidth;
+
+    [FieldOffset(sizeof(ushort))]
+    public ushort DefaultHeight;
+
+    [FieldOffset(sizeof(ushort))]
+    public ushort BlendMode;
+
+    [FieldOffset(sizeof(ushort))]
+    public byte Opacity;
+
+    [FieldOffset(sizeof(byte))]
+    public byte[] Ignore;
+
+    [FieldOffset(sizeof(byte) * 3)]
+    public ushort NameLen;
+}

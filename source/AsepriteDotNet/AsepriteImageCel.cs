8c921b9ee59b8a4caf9777fa3f8b1c3b575bf7a0
Christopher Whitley <aristurtledev@gmail.com>
2024-02-20 00:17:43 -0500

    Remove System.Drawing dependency

--- source/AsepriteDotNet/AsepriteImageCel.cs
+++ source/AsepriteDotNet/AsepriteImageCel.cs
@@ -2,8 +2,6 @@
 //  Licensed under the MIT license.
 //  See LICENSE file in the project root for full license information.
 
-using System.Drawing;
-
 namespace AsepriteDotNet;
 
 /// <summary>
@@ -14,12 +12,17 @@ public sealed class AsepriteImageCel : AsepriteCel
     private readonly AseColor[] _pixels;
 
     /// <summary>
-    /// The size of this <see cref="AsepriteImageCel"/>, in pixels.
+    /// Gets the width of this <see cref="AsepriteImageCel"/>, in pixels.
     /// </summary>
-    public Size Size { get; }
+    public int Width { get; }
 
     /// <summary>
-    /// A collection of the <see cref="Color"/> elements that represents the pixel data that makes up the image
+    /// Gets the height of this <see cref="AsepriteImageCel"/>, in pixels.
+    /// </summary>
+    public int Height { get; }
+
+    /// <summary>
+    /// A collection of the <see cref="AseColor"/> elements that represents the pixel data that makes up the image
     /// for this <see cref="AsepriteCel"/>.  Order of the color elements starts with the top-left most pixel and is read
     /// left-to-right from top-to-bottom.
     /// </summary>
@@ -28,7 +31,8 @@ public sealed class AsepriteImageCel : AsepriteCel
     internal AsepriteImageCel(CelProperties celProperties, AsepriteLayer layer, ImageCelProperties imageCelProperties, AseColor[] pixels)
         : base(celProperties, layer)
     {
-        Size = new Size(imageCelProperties.Width, imageCelProperties.Height);
+        Width = imageCelProperties.Width;
+        Height = imageCelProperties.Height;
         _pixels = pixels;
     }
 }

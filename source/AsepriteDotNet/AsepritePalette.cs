0269d475a9e0ac8e464d0a59aff3f99e11280ff8
Christopher Whitley <aristurtledev@gmail.com>
2024-02-21 02:07:20 -0500

    Update functionality

--- source/AsepriteDotNet/AsepritePalette.cs
+++ source/AsepriteDotNet/AsepritePalette.cs
@@ -2,34 +2,82 @@
 //  Licensed under the MIT license.
 //  See LICENSE file in the project root for full license information.
 
+using System.Collections;
+
 namespace AsepriteDotNet;
 
 /// <summary>
 /// Represents the palette in an Aseprite file.
 /// </summary>
-public sealed class AsepritePalette
+public sealed class AsepritePalette : IEnumerable<AseColor>
 {
-    private readonly AseColor[] _colors;
+    private AseColor[] _colors = Array.Empty<AseColor>();
 
     /// <summary>
-    /// An array of <see cref="AseColor"/> color elements that represent the colors of the palette.  Order of elements is
-    /// the same as the order of colors in the palette in Aseprite.
+    /// Gets the <see cref="AseColor"/> element at the specified index from this <see cref="AsepritePalette"/>.
+    /// </summary>
+    /// <param name="index">The index of the <see cref="AseColor"/> element.</param>
+    /// <returns>The <see cref="AseColor"/> element at the specified <paramref name="index"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Thrown if the <paramref name="index"/> is less than zero or greater than or equal to <see cref="Count"/>.
+    /// </exception>
+    public AseColor this[int index]
+    {
+        get
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(index);
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, _colors.Length);
+            return _colors[index];
+        }
+
+        internal set
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(index);
+            ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, _colors.Length);
+            _colors[index] = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets the index of the <see cref="AseColor"/> element in this <see cref="AsepritePalette"/> that should be
+    /// interpreted as a transparent color.
+    /// </summary>
+    public int TransparentIndex { get; }
+
+    /// <summary>
+    /// Gets the total number of <see cref="AseColor"/> elements in this <see cref="AsepritePalette"/>.
+    /// </summary>
+    public int Count => _colors.Length;
+
+    /// <summary>
+    /// Gets a <see cref="ReadOnlySpan{T}"/> of the <see cref="AseColor"/> elements in this 
+    /// <see cref="AsepritePalette"/>.  Order of elements is the same as the order of colors in the palette in Aseprite.
     /// </summary>
     public ReadOnlySpan<AseColor> Colors => _colors;
 
-    /// <summary>
-    /// The index of the element in <see cref="Colors"/> that represents a color that should be interpreted as a
-    /// transparent color.
-    /// </summary>
-    /// <remarks>
-    /// This value is only valid when the <see cref="AsepriteColorDepth"/> mode used is
-    /// <see cref="AsepriteColorDepth.Indexed"/>.
-    /// </remarks>
-    public int TransparentIndex { get; }
+    internal AsepritePalette(int transparentIndex) => TransparentIndex = transparentIndex;
 
-    internal AsepritePalette(AseColor[] colors, int transparentIndex)
+    internal void Resize(int newSize)
     {
-        _colors = colors;
-        TransparentIndex = transparentIndex;
+        AseColor[] newColors = new AseColor[newSize];
+        Array.Copy(_colors, newColors, _colors.Length);
+        _colors = newColors;
     }
+
+    /// <summary>
+    /// Returns an enumerator that iterates through the <see cref="AseColor"/> elements in this 
+    /// <see cref="AsepritePalette"/>.
+    /// </summary>
+    /// <returns>
+    /// An enumerator that iterates through the <see cref="AseColor"/> elements in this <see cref="AsepritePalette"/>.
+    /// </returns>
+    public IEnumerator<AseColor> GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }

4f04b0b33d66cc0569424df8010e5ac9cbd8d525
Christopher Whitley <aristurtledev@gmail.com>
2024-02-21 02:27:02 -0500

    Added null check properties

--- source/AsepriteDotNet/AsepriteUserData.cs
+++ source/AsepriteDotNet/AsepriteUserData.cs
@@ -2,6 +2,8 @@
 //  Licensed under the MIT license.
 //  See LICENSE file in the project root for full license information
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace AsepriteDotNet;
 
 /// <summary>
@@ -9,6 +11,18 @@ namespace AsepriteDotNet;
 /// </summary>
 public sealed class AsepriteUserData
 {
+    /// <summary>
+    /// Gets a value that indicates whether text was set for this <see cref="AsepriteUserData"/> in Aseprite.
+    /// </summary>
+    [MemberNotNullWhen(true, nameof(Text))]
+    public bool HasText => Text is not null;
+
+    /// <summary>
+    /// Gets a value that indicates whether a color value was set for this <see cref="AsepriteUserData"/> in Aseprite.
+    /// </summary>
+    [MemberNotNullWhen(true, nameof(Color))]
+    public bool HasColor => Color is not null;
+
     /// <summary>
     /// Gets the text that was set for this user data in Aseprite; if text was set; otherwise, <see langword="null"/>.
     /// </summary>
@@ -20,6 +34,9 @@ public sealed class AsepriteUserData
     /// </summary>
     public AseColor? Color { get; internal set; }
 
-    internal AsepriteUserData() : this(null, null) { }
-    internal AsepriteUserData(string? text, AseColor? color) => (Text, Color) = (text, color);
+    internal AsepriteUserData()
+    {
+        Text = null;
+        Color = null;
+    }
 }

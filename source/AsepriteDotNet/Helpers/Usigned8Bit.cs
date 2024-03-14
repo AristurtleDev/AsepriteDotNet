ae2c3a395ffdb56a5f4314ec5da0fa7a8fe66bbd
Christopher Whitley <aristurtledev@gmail.com>
2024-03-14 17:04:25 -0400

    Added Unsigned8Bit helper class This is used to multiply and divide two unsigned 8-bit values.

---
+++ source/AsepriteDotNet/Helpers/Usigned8Bit.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace AsepriteDotNet.Helpers;
+
+internal static class Usigned8Bit
+{
+    public static byte Multiply(int a, int b)
+    {
+        int v = (a * b) + 0x80;
+        return (byte)(((v >> 8) + v) >> 8);
+    }
+
+    public static byte Divide(int a, int b) => (byte)(((ushort)a * 0xFF + (b / 2)) / b);
+}

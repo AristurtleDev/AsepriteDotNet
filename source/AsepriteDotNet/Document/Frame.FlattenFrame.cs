646569909ad760256bf76b98c662f78a368befed
Christopher Whitley <aristurtledev@gmail.com>
2024-03-15 01:31:46 -0400

    Initial implementation of FlattenFrame, needs testing

--- source/AsepriteDotNet/Document/Frame.FlattenFrame.cs
+++ source/AsepriteDotNet/Document/Frame.FlattenFrame.cs
@@ -1,60 +1,65 @@
-﻿//// Copyright (c) Christopher Whitley. All rights reserved.
-//// Licensed under the MIT license.
-//// See LICENSE file in the project root for full license information.
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Security.Principal;
-//using System.Text;
-//using System.Threading.Tasks;
-//using AsepriteDotNet.Helpers;
+using AsepriteDotNet.Helpers;
 
-//namespace AsepriteDotNet.Document;
+namespace AsepriteDotNet.Document;
 
-//public static class FrameExtensions
-//{
-//    public static AseColor[] FlattenFrame(this Frame frame, bool onlyVisibleLayers)
-//    {
-//        return frame.FlattenFrame<AseColor>(onlyVisibleLayers, (color) => color);
-//    }
+public static class FrameExtensions
+{
+    public static AseColor[] FlattenFrame(this Frame frame, bool onlyVisibleLayers)
+    {
+        return frame.FlattenFrame<AseColor>(onlyVisibleLayers, (color) => color);
+    }
 
-//    public static T[] FlattenFrame<T>(this Frame frame, bool onlyVisibleLayers, Func<AseColor, T> colorProcessor) where T : struct
-//    {
-//        AseColor[] result = new AseColor[frame.Width * frame.Height];
-//        ReadOnlySpan<Cel> cels = frame.Cels;
+    public static T[] FlattenFrame<T>(this Frame frame, bool onlyVisibleLayers, Func<AseColor, T> colorProcessor) where T : struct
+    {
+        ArgumentNullException.ThrowIfNull(frame);
+        ArgumentNullException.ThrowIfNull(colorProcessor);
 
-//        for(int celNum = 0; celNum < cels.Length; celNum++)
-//        {
-//            Cel cel = cels[celNum];
-//            if(cel is LinkedCel linkedCel)
-//            {
-//                cel = linkedCel.Cel;
-//            }
+        AseColor[] flattened = new AseColor[frame.Width * frame.Height];
+        ReadOnlySpan<Cel> cels = frame.Cels;
 
-//            if (cel is not ImageCel imageCel) { continue; }
-//            if (onlyVisibleLayers && !imageCel.Layer.IsVisible) { continue; }
+        for (int celNum = 0; celNum < cels.Length; celNum++)
+        {
+            Cel cel = cels[celNum];
+            if (cel is LinkedCel linkedCel)
+            {
+                cel = linkedCel.Cel;
+            }
 
-//            ReadOnlySpan<AseColor> pixels = imageCel.Pixels;
-//            byte opacity = Usigned8Bit.Multiply(imageCel.Opacity, imageCel.Layer.Opacity);
+            if (cel is not ImageCel imageCel) { continue; }
+            if (onlyVisibleLayers && !imageCel.Layer.IsVisible) { continue; }
 
-//            for(int pixelNum = 0; pixelNum < pixels.Length; pixelNum++)
-//            {
-//                int x = (pixelNum % imageCel.Width) + imageCel.X;
-//                int y = (pixelNum / imageCel.Width) + imageCel.Y;
-//                int index = y * frame.Width + x;
+            ReadOnlySpan<AseColor> pixels = imageCel.Pixels;
+            byte opacity = imageCel.Opacity.MUL_UN8(imageCel.Layer.Opacity);
 
-//                //  Sometimes a cel can have a negative x and/or y value.  This is caused by selecting an area within
-//                //  aseprite and then moving a portion of the selected pixels outside the canvas.  We don't care about
-//                //  these pixels, so if the index is outside the range of the array to store them in, we'll just
-//                //  discard them
-//                if (index < 0 || index > result.Length) { continue; }
+            for (int pixelNum = 0; pixelNum < pixels.Length; pixelNum++)
+            {
+                int x = (pixelNum % imageCel.Width) + imageCel.X;
+                int y = (pixelNum / imageCel.Width) + imageCel.Y;
+                int index = y * frame.Width + x;
 
-//                AseColor background = result[index];
-//                AseColor source = pixels[pixelNum];
-//                AseColor blended = 
-//            }
-//        }
-//    }
-//}
+                //  Sometimes a cel can have a negative x and/or y value.  This is caused by selecting an area within
+                //  aseprite and then moving a portion of the selected pixels outside the canvas.  We don't care about
+                //  these pixels, so if the index is outside the range of the array to store them in, we'll just
+                //  discard them
+                if (index < 0 || index > flattened.Length) { continue; }
+
+                AseColor backdrop = flattened[index];
+                AseColor source = pixels[pixelNum];
+                flattened[index] = backdrop.Blend(source, opacity, imageCel.Layer.BlendMode);
+            }
+        }
+
+        T[] result = new T[flattened.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = colorProcessor(flattened[i]);
+        }
+
+        return result;
+    }
+}
 

0b4a434a4d0093ff21ff38c6913e5e82f3bdcce7
Christopher Whitley <aristurtledev@gmail.com>
2024-03-14 23:01:10 -0400

    Added Normal blending

---
+++ source/AsepriteDotNet/AseColor.Blend.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using System.Drawing;
+using AsepriteDotNet.Helpers;
+
+namespace AsepriteDotNet;
+
+public static class AseColorBlending
+{
+    private const byte RGBA_R_SHIFT = 0;
+    private const byte RGBA_G_SHIFT = 8;
+    private const byte RGBA_B_SHIFT = 16;
+    private const byte RGBA_A_SHIFT = 24;
+    private const uint RGBA_R_MASK = 0x000000ff;
+    private const uint RGBA_G_MASK = 0x0000ff00;
+    private const uint RGBA_B_MASK = 0x00ff0000;
+    private const uint RGBA_RGB_MASK = 0x00ffffff;
+    private const uint RGBA_A_MASK = 0xff000000;
+
+    public static AseColor Blend(this AseColor backdrop, AseColor source, int opacity, AsepriteBlendMode blendMode) =>
+        backdrop.Blend(source, opacity, blendMode, (color) => color);
+
+    public static T Blend<T>(this AseColor backdrop, AseColor source, int opacity, AsepriteBlendMode blendMode, Func<AseColor, T> converter)
+    {
+        //  Exit early depending on alpha
+        if (backdrop.A == 0 && source.A == 0) { return converter(new AseColor(0, 0, 0, 0)); }
+        if (backdrop.A == 0) { return converter(source); }
+        if (source.A == 0) { return converter(backdrop); }
+
+        AseColor blended = blendMode switch
+        {
+            #pragma warning disable format
+            AsepriteBlendMode.Normal        => Normal(backdrop, source, opacity),
+            //AsepriteBlendMode.Multiply      => Multiply(backdrop, source, opacity),
+            //AsepriteBlendMode.Screen        => Screen(backdrop, source, opacity),
+            //AsepriteBlendMode.Overlay       => Overlay(backdrop, source, opacity),
+            //AsepriteBlendMode.Darken        => Darken(backdrop, source, opacity),
+            //AsepriteBlendMode.Lighten       => Lighten(backdrop, source, opacity),
+            //AsepriteBlendMode.ColorDodge    => ColorDodge(backdrop, source, opacity),
+            //AsepriteBlendMode.ColorBurn     => ColorBurn(backdrop, source, opacity),
+            //AsepriteBlendMode.HardLight     => HardLight(backdrop, source, opacity),
+            //AsepriteBlendMode.SoftLight     => SoftLight(backdrop, source, opacity),
+            //AsepriteBlendMode.Difference    => Difference(backdrop, source, opacity),
+            //AsepriteBlendMode.Exclusion     => Exclusion(backdrop, source, opacity),
+            //AsepriteBlendMode.Hue           => HslHue(backdrop, source, opacity),
+            //AsepriteBlendMode.Saturation    => HslSaturation(backdrop, source, opacity),
+            //AsepriteBlendMode.Color         => HslColor(backdrop, source, opacity),
+            //AsepriteBlendMode.Luminosity    => HslLuminosity(backdrop, source, opacity),
+            //AsepriteBlendMode.Addition      => Addition(backdrop, source, opacity),
+            //AsepriteBlendMode.Subtract      => Subtract(backdrop, source, opacity),
+            //AsepriteBlendMode.Divide        => Divide(backdrop, source, opacity),
+            _                               => throw new InvalidOperationException($"Unknown blend mode '{blendMode}'")
+            #pragma warning restore format           
+        };
+
+        return converter(blended);
+    }
+
+    private static AseColor Normal(AseColor backdrop, AseColor source, int opacity)
+    {
+        if ((backdrop.PackedValue & RGBA_A_MASK) == 0)
+        {
+            source.A = Unsigned8Bit.Multiply(source.A, opacity);
+            source.A <<= RGBA_A_SHIFT;
+            return source;
+        }
+        else if ((source.PackedValue & RGBA_A_MASK) == 0)
+        {
+            return backdrop;
+        }
+
+        opacity = Unsigned8Bit.Multiply(source.A, opacity);
+
+        int a = source.A + backdrop.A - Unsigned8Bit.Multiply(backdrop.A, source.A);
+        int r = backdrop.R + (source.R - backdrop.R) * opacity / a;
+        int g = backdrop.G + (source.G - backdrop.G) * opacity / a;
+        int b = backdrop.B + (source.B - backdrop.B) * opacity / a;
+
+        return new AseColor((byte)r, (byte)g, (byte)b, (byte)a);
+    }
+
+
+
+
+
+
+}
---
+++ tests/AsepriteDotNet.Tests/AseColorTests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace AsepriteDotNet.Tests
+{
+    public sealed class AseColorTests
+    {
+        private static readonly AseColor _green = new AseColor(106, 190, 48, 255);
+        private static readonly AseColor _orange = new AseColor(223, 113, 38, 255);
+        private static readonly AseColor _transparent = new AseColor(0, 0, 0, 0);
+        private static readonly AseColor _purple = new AseColor(63, 63, 116, 255);
+        private static readonly AseColor _pink = new AseColor(215, 123, 186, 255);
+        private static readonly AseColor _red = new AseColor(172, 50, 50, 255);
+
+        #region Normal Blend
+
+        [Fact]
+        public void AseColor_NormalBlend_Color_On_Color_Test()
+        {
+            AsepriteBlendMode mode = AsepriteBlendMode.Normal;
+            Assert.Equal(_orange, _green.Blend(_orange, 255, mode));
+        }
+
+        [Fact]
+        public void AseColor_NormalBlend_Color_On_Transparent_Test()
+        {
+            AsepriteBlendMode mode = AsepriteBlendMode.Normal;
+            Assert.Equal(_orange, _transparent.Blend(_orange, 255, mode));
+        }
+
+        [Fact]
+        public void AseColor_NormalBLend_Transparent_On_Color_Test()
+        {
+            AsepriteBlendMode mode = AsepriteBlendMode.Normal;
+            Assert.Equal(_green, _green.Blend(_transparent, 255, mode));
+        }
+
+        [Fact]
+        public void AseColor_NormalBlend_Transparent_On_Transparent_Test()
+        {
+            AsepriteBlendMode mode = AsepriteBlendMode.Normal;
+            Assert.Equal(_transparent, _transparent.Blend(_transparent, 255, mode));
+        }
+
+        #endregion Normal Blend
+
+
+
+    }
+}

e23f8322aae906de1c2e0e1bd5a856ac4847646d
Christopher Whitley <aristurtledev@gmail.com>
2024-03-25 00:03:41 -0400

    Added examples

---
+++ examples/FlattenFrameExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using AsepriteDotNet.Aseprite;
+using AsepriteDotNet.Aseprite.Types;
+using AsepriteDotNet.Common;
+using AsepriteDotNet.IO;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Load the file using the AsepriteFileLoader.  In this example, we are passing the path to the file.  There is also
+/// an overload where you can pass a stream instead if you needed.
+/// 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+AsepriteFile aseFile = AsepriteFileLoader.FromFile("adventurer.aseprite");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Flatten a single frame to get the pixel representation of that frame.
+/// 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Rgba32[] frame0Pixels = aseFile.Frames[0].FlattenFrame(onlyVisibleLayers: true, includeBackgroundLayer: false, includeTilemapCels: true);
+
+
+//  At this point you can use the pixel data of that frame in your application ever how you need it.  
---
+++ examples/LoadFileExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using AsepriteDotNet.Aseprite;
+using AsepriteDotNet.IO;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Load the file using the AsepriteFileLoader.  In this example, we are passing the path to the file.  There is also
+/// an overload where you can pass a stream instead if you needed.
+/// 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+AsepriteFile aseFile = AsepriteFileLoader.FromFile("adventurer.aseprite");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Output information about the file read.
+/// 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+string info =
+$"""
+    Filename:       {aseFile.Name}
+    Color Depth:    {aseFile.ColorDepth}
+    Palette Size:   {aseFile.Palette.Count}
+    Canvas Size:    {aseFile.CanvasWidth} x {aseFile.CanvasHeight}
+    Frame Count:    {aseFile.Frames.Length}
+    Layers Count:   {aseFile.Layers.Length}
+    Slices Count:   {aseFile.Slices.Length}
+    Tags Count:     {aseFile.Tags.Length}
+    Tileset Count:  {aseFile.Tilesets.Length}
+    """;
+
+Console.WriteLine(info);

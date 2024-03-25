a7654f02d38e03590982e809bbea40ececd95a81
Christopher Whitley <aristurtledev@gmail.com>
2024-03-25 13:49:06 -0400

    Added license header

--- examples/ProcessorExample/Program.cs
+++ examples/ProcessorExample/Program.cs
@@ -1,4 +1,8 @@
-﻿using AsepriteDotNet;
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using AsepriteDotNet;
 using AsepriteDotNet.Aseprite;
 using AsepriteDotNet.IO;
 using AsepriteDotNet.Processors;

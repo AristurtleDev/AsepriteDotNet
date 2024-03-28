efe2cee7a1cd3887bd369564727d14ab6245d759
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2024-03-28 00:03:28 -0400

    Added `ExtractCel` method to extract texture from individual cels (#24)
    * Adds ExtractCel method
    ExtractCel methods allow consumers to extract the contents of a single cel into a texture object without having to flatten the entire frame.
    
    * Remove invalid character
    
    * Add example of using ExtractCel
    
    * Bump version to 1.6.0

---
+++ examples/ExtractCelExample/Program.cs
@@ -0,0 +1,7 @@
+﻿using AsepriteDotNet;
+using AsepriteDotNet.Aseprite;
+using AsepriteDotNet.IO;
+
+AsepriteFile aseFile = AsepriteFileLoader.FromFile("adventurer.aseprite");
+Texture head = aseFile.ExtractCel(0, "head");
+PngWriter.SaveTo("head.png", head.Size.Width, head.Size.Height, head.Pixels.ToArray());

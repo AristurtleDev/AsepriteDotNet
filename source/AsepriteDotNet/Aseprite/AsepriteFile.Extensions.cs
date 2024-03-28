45ec24adb6334a5fcf6ee4cb02c9bc95a2f90c4b
Christopher Whitley <aristurtledev@gmail.com>
2024-03-27 23:55:18 -0400

    Adds ExtractCel method ExtractCel methods allow consumers to extract the contents of a single cel into a texture object without having to flatten the entire frame.

---
+++ source/AsepriteDotNet/Aseprite/AsepriteFile.Extensions.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using AsepriteDotNet.Aseprite.Types;
+
+namespace AsepriteDotNet.Aseprite;
+
+/// <summary>
+/// Provides extension methods for <see cref="AsepriteFile"/> instances.
+/// </summary>
+public static class AsepriteFileExtensions
+{
+    /// <summary>
+    /// Extracts pixel data from a specific cel in a specified frame of an <see cref="AsepriteFile"/> and returns it as
+    /// a <see cref="Texture"/>.
+    /// </summary>
+    /// <remarks>
+    /// Cel index of a frame starts at zero with the bottom most and goes up.  If a layer in a frame does not have cel
+    /// data, then the frame does not have that cel.  For instance, if your Aseprite file has 10 layers, but frame 0
+    /// does not have pixels on layer 2, then frame 0 will only have 9 cels and not the full 10 cels.
+    ///
+    /// Due to this, it may be easier to use the <see cref="ExtractCel(AsepriteFile, int, string, string?)"/> method
+    /// to specify the layer name to extract the cel from.
+    /// </remarks>
+    /// <param name="file">
+    /// The <see cref="AsepriteFile"/> containing the frame and cel from which to extract the pixel data.
+    /// </param>
+    /// <param name="frameIndex">
+    /// The index of the frame containing the cel from which to extract the pixel data.
+    /// </param>
+    /// <param name="celIndex">
+    /// The index of the cel within the specified frame from which to extract the pixel data.
+    /// </param>
+    /// <param name="name">
+    /// Optional name for the extracted texture. If not provided, a default name is generated.
+    /// </param>
+    /// <returns>A <see cref="Texture"/> object containing the extracted pixel data.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the input <see cref="AsepriteFile"/> is null.</exception>
+    public static Texture ExtractCel(this AsepriteFile file, int frameIndex, int celIndex, string? name = null)
+    {
+        ArgumentNullException.ThrowIfNull(file);
+        name ??= $"{file.Name}_frame{frameIndex}_cel{celIndex}";
+        AsepriteCel cel = file.Frames[frameIndex].Cels[celIndex];
+        return cel.ExtractCel(name);
+
+    }
+
+    /// <summary>
+    /// Extracts pixel data from a specific layer in a specified frame of an <see cref="AsepriteFile"/> and returns it
+    /// as a <see cref="Texture"/>.
+    /// </summary>
+    /// <param name="file">
+    /// The <see cref="AsepriteFile"/> containing the frame and layer from which to extract the pixel data.
+    /// </param>
+    /// <param name="frameIndex">The index of the frame from which to extract the pixel data.</param>
+    /// <param name="layerName">The name of the layer from which to extract the pixel data.</param>
+    /// <param name="name">Optional name for the extracted texture. If not provided, a default name is generated.</param>
+    /// <returns>A <see cref="Texture"/>. object containing the extracted pixel data.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when the input <see cref="AsepriteFile"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">Thrown when the specified layer cannot be located.</exception>
+    public static Texture ExtractCel(this AsepriteFile file, int frameIndex, string layerName, string? name = null)
+    {
+        ArgumentNullException.ThrowIfNull(file);
+        name ??= $"{file.Name}_frame{frameIndex}_{layerName}_cel";
+        AsepriteCel? cel = null;
+        AsepriteFrame frame = file.Frames[frameIndex];
+        Parallel.For(0, frame.Cels.Length, new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, (i, loopState) =>
+        {
+            if (frame.Cels[i].Layer.Name.Equals(layerName, StringComparison.Ordinal))
+            {
+                cel = frame.Cels[i];
+                loopState.Break();
+            }
+        });
+
+        if (cel is null)
+        {
+            throw new ArgumentException($"Unable to locate cel in frame {frameIndex} on a layer called '{layerName}'", layerName);
+        }
+
+        return cel.ExtractCel(name);
+    }
+}
---
+++ source/AsepriteDotNet/Aseprite/Types/AsepriteCel.Extensions.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using AsepriteDotNet.Common;
+
+namespace AsepriteDotNet.Aseprite.Types;
+
+/// <summary>
+/// Provides extension methods for <see cref="AsepriteCel"/> instances.
+/// </summary>
+public static class AsepriteCelExtensions
+{
+    /// <summary>
+    /// Extracts pixel data from an <see cref="AsepriteCel"/> and returns it as a <see cref="Texture"/>.
+    /// </summary>
+    /// <param name="cel">The <see cref="AsepriteCel"/> containing the pixel data to extract.</param>
+    /// <param name="name">
+    /// Optional name for the extracted <see cref="Texture"/>. If not provided, an empty string is used.
+    /// </param>
+    /// <returns>A <see cref="Texture"/> object containing the extracted pixel data.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when the input <see cref="AsepriteCel"/> is <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown when the <see cref="AsepriteCel"/> does not contain pixel data.
+    /// </exception>
+    public static Texture ExtractCel(this AsepriteCel cel, string? name = null)
+    {
+        ArgumentNullException.ThrowIfNull(cel);
+
+
+        if (cel is AsepriteImageCel imageCel)
+        {
+            return imageCel.ExtractCel(name);
+        }
+
+        if (cel is AsepriteTilemapCel tilemapCel)
+        {
+            return tilemapCel.ExtractCel(name);
+        }
+
+        throw new InvalidOperationException("This cel does not contain pixel data");
+    }
+
+    {
+    /// <summary>
+    /// Extracts pixel data from an <see cref="AsepriteImageCel"/> and returns it as a <see cref="Texture"/>.
+    /// </summary>
+    /// <param name="cel">The <see cref="AsepriteImageCel"/> containing the pixel data to extract.</param>
+    /// <param name="name">
+    /// Optional name for the extracted <see cref="Texture"/>. If not provided, an empty string is used.
+    /// </param>
+    /// <returns>A <see cref="Texture"/> object containing the extracted pixel data.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when the input <see cref="AsepriteImageCel"/> is <see langword="null"/>.
+    /// </exception>
+    public static Texture ExtractCel(this AsepriteImageCel cel, string? name = null)
+    {
+        ArgumentNullException.ThrowIfNull(cel);
+
+        return new Texture(name ?? string.Empty, cel.Size, cel.Pixels.ToArray());
+    }
+
+    /// <summary>
+    /// Extracts pixel data from an <see cref="AsepriteTilemapCel"/> and returns it as a <see cref="Texture"/>.
+    /// </summary>
+    /// <param name="cel">The <see cref="AsepriteTilemapCel"/> containing the pixel data to extract.</param>
+    /// <param name="name">
+    /// Optional name for the extracted <see cref="Texture"/>. If not provided, an empty string is used.
+    /// </param>
+    /// <returns>A <see cref="Texture"/> object containing the extracted pixel data.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when the input <see cref="AsepriteTilemapCel"/> is <see langword="null"/>.
+    /// </exception>
+    public static Texture ExtractCel(this AsepriteTilemapCel cel, string? name = null)
+    {
+        ArgumentNullException.ThrowIfNull(cel);
+
+        AsepriteTilemapLayer layer = (AsepriteTilemapLayer)cel.Layer;
+        AsepriteTileset tileset = layer.Tileset;
+        Size size = new Size(cel.Size.Width * tileset.TileSize.Width, cel.Size.Height * tileset.TileSize.Height);
+        Rgba32[] pixels = new Rgba32[size.Width * size.Height];
+
+        Parallel.For(0, cel.Tiles.Length, new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, i =>
+        {
+            AsepriteTile tile = cel.Tiles[i];
+            int column = i % cel.Size.Width;
+            int row = i / cel.Size.Width;
+            Parallel.For(0, tileset.Pixels.Length, new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, j =>
+            {
+                int px = (j % tileset.TileSize.Width) + (column + tileset.TileSize.Height);
+                int py = (j / tileset.TileSize.Width) + (row + tileset.TileSize.Height);
+                int index = py * size.Width + px;
+                pixels[index] = tileset.Pixels[j];
+            });
+        });
+
+        return new Texture(name ?? string.Empty, size, pixels);
+    }
+}

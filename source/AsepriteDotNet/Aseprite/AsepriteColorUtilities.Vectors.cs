9acb5d610e8470e6d4c8e64d06897fe4a5cc0e8f
Christopher Whitley <aristurtledev@gmail.com>
2024-04-15 17:46:43 -0400

    Added vector versions of methods

---
+++ source/AsepriteDotNet/Aseprite/AsepriteColorUtilities.Vectors.cs
@@ -0,0 +1,279 @@
+﻿// Copyright (c) Christopher Whitley. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+using System.Numerics;
+using AsepriteDotNet.Aseprite;
+using AsepriteDotNet.Aseprite.Types;
+using AsepriteDotNet.Common;
+using AsepriteDotNet.Compression;
+
+namespace AsepriteDotNet;
+
+internal static class AsepriteColorUtilitiesVectors
+{
+    /// <summary>
+    /// Converts an array of <see cref="byte"/> data to an array of <see cref="Rgba32"/> values based on the
+    /// specified <see cref="AsepriteColorDepth"/>.
+    /// </summary>
+    /// <param name="pixels">The array of <see cref="byte"/> data that represents the color data.</param>
+    /// <param name="depth">The color depth.</param>
+    /// <param name="preMultiplyAlpha">
+    /// Indicates whether color values should be translated to a premultiplied alpha value.
+    /// </param>
+    /// <param name="palette">
+    /// The palette used for <see cref="AsepriteColorDepth.Indexed">ColorDepth.Index</see>.  Optional, only required when
+    /// <paramref name="depth"/> is equal to <see cref="AsepriteColorDepth.Indexed">ColorDepth.Indexed</see>.
+    /// </param>
+    /// <returns>An array of <see cref="Rgba32"/> values converted from the data.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="pixels"/> is <see langword="null"/>.</exception>
+    /// <exception cref="InvalidOperationException">
+    /// <paramref name="depth"/> is an unknown <see cref="AsepriteColorDepth"/> value.
+    /// </exception>
+    internal static Rgba32[] PixelsToColor(byte[] pixels, AsepriteColorDepth depth, bool preMultiplyAlpha, AsepritePalette? palette = null)
+    {
+        ArgumentNullException.ThrowIfNull(pixels);
+
+        int bpp = (int)depth / 8;
+        Rgba32[] result = new Rgba32[pixels.Length / bpp];
+
+        for (int i = 0, b = 0; i < result.Length; i++, b += bpp)
+        {
+            byte red, green, blue, alpha;
+            red = green = blue = alpha = 0;
+
+            switch (depth)
+            {
+                case AsepriteColorDepth.RGBA:
+                    red = pixels[b];
+                    green = pixels[b + 1];
+                    blue = pixels[b + 2];
+                    alpha = pixels[b + 3];
+                    break;
+
+                case AsepriteColorDepth.Grayscale:
+                    red = green = blue = pixels[b];
+                    alpha = pixels[b + 1];
+                    break;
+
+                case AsepriteColorDepth.Indexed:
+                    int index = pixels[i];
+                    if (index != palette?.TransparentIndex)
+                    {
+                        if(palette is not null)
+                        {
+                            palette.Colors[index].Deconstruct(out red, out green, out blue, out alpha);
+                        }
+                    }
+                    break;
+
+                default:
+                    throw new InvalidOperationException($"Unknown Color Depth: {depth}");
+            }
+            result[i] = preMultiplyAlpha ? Rgba32.FromNonPreMultiplied(red, green, blue, alpha) : new Rgba32(red, green, blue, alpha);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Calculates the saturation value based on the given RGB color component values.
+    /// </summary>
+    /// <param name="r">The red color component value (0 to 1).</param>
+    /// <param name="g">The green color component value (0 to 1).</param>
+    /// <param name="b">The blue color component value (0 to 1).</param>
+    /// <returns>The saturation value calculated.</returns>
+    internal static double CalculateSaturation(double r, double g, double b)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Calculates the luminance value based on the given RGB color component values.
+    /// </summary>
+    /// <param name="r">The red color component value (0 to 1).</param>
+    /// <param name="g">The green color component value (0 to 1).</param>
+    /// <param name="b">The blue color component value (0 to 1).</param>
+    /// <returns></returns>
+    internal static double CalculateLuminance(double r, double g, double b)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Modifies the saturation of the specified RGB color component values.
+    /// </summary>
+    /// <param name="r">The red color component value (0 to 1).</param>
+    /// <param name="g">The green color component value (0 to 1).</param>
+    /// <param name="b">The blue color component value (0 to 1).</param>
+    /// <param name="s">The saturation factor to adjust the color components by.</param>
+    internal static void AdjustSaturation(ref double r, ref double g, ref double b, double s)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Modifies the luminosity of the specified RGB color component values.
+    /// </summary>
+    /// <param name="r">The red color component value (0 to 1).</param>
+    /// <param name="g">The green color component value (0 to 1).</param>
+    /// <param name="b">The blue color component value (0 to 1).</param>
+    /// <param name="l">The desired luminosity value to apply.</param>
+    internal static void AdjustLumanice(ref double r, ref double g, ref double b, double l)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Normalizes the specified RGB color component values to ensure they are within the valid range of 0 to 1.
+    /// Clips the values of the specified RGB color
+    /// </summary>
+    /// <param name="r">The red color component value.</param>
+    /// <param name="g">The green color component value.</param>
+    /// <param name="b">The blue color component value.</param>
+    internal static void NormalizeColor(ref double r, ref double g, ref double b)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Blends two <see cref="Rgba32"/> values using the specified <see cref="AsepriteBlendMode"/> and opacity.
+    /// </summary>
+    /// <param name="backdrop">The backdrop color.</param>
+    /// <param name="source">The source color to be blended onto the <paramref name="backdrop"/>.</param>
+    /// <param name="opacity">The opacity of the blending operation.</param>
+    /// <param name="blendMode">The <see cref="AsepriteBlendMode"/> to use for the blending operation.</param>
+    /// <returns>The resulting <see cref="Rgba32"/> value created from the blending.</returns>
+    /// <exception cref="InvalidOperationException">
+    /// <paramref name="blendMode"/> is an unknown <see cref="AsepriteBlendMode"/> value.
+    /// </exception>
+    internal static Vector4[] Blend(Vector4[] backdrop, Vector4[] source, int opacity, AsepriteBlendMode blendMode)
+    {
+        //  The original method performs alpha check on backdrop and source and does an early exit
+        //  based on that.  Not sure how you would implement that here?
+        //  See line 200 in AsepriteColorUtilities.cs
+
+        Vector4[] blended = blendMode switch
+        {
+            #pragma warning disable format
+            AsepriteBlendMode.Normal        => Normal(backdrop, source, opacity),
+            AsepriteBlendMode.Multiply      => Multiply(backdrop, source, opacity),
+            AsepriteBlendMode.Screen        => Screen(backdrop, source, opacity),
+            AsepriteBlendMode.Overlay       => Overlay(backdrop, source, opacity),
+            AsepriteBlendMode.Darken        => Darken(backdrop, source, opacity),
+            AsepriteBlendMode.Lighten       => Lighten(backdrop, source, opacity),
+            AsepriteBlendMode.ColorDodge    => ColorDodge(backdrop, source, opacity),
+            AsepriteBlendMode.ColorBurn     => ColorBurn(backdrop, source, opacity),
+            AsepriteBlendMode.HardLight     => HardLight(backdrop, source, opacity),
+            AsepriteBlendMode.SoftLight     => SoftLight(backdrop, source, opacity),
+            AsepriteBlendMode.Difference    => Difference(backdrop, source, opacity),
+            AsepriteBlendMode.Exclusion     => Exclusion(backdrop, source, opacity),
+            AsepriteBlendMode.Hue           => HslHue(backdrop, source, opacity),
+            AsepriteBlendMode.Saturation    => HslSaturation(backdrop, source, opacity),
+            AsepriteBlendMode.Color         => HslColor(backdrop, source, opacity),
+            AsepriteBlendMode.Luminosity    => HslLuminosity(backdrop, source, opacity),
+            AsepriteBlendMode.Addition      => Addition(backdrop, source, opacity),
+            AsepriteBlendMode.Subtract      => Subtract(backdrop, source, opacity),
+            AsepriteBlendMode.Divide        => Divide(backdrop, source, opacity),
+            _                               => throw new InvalidOperationException($"Unknown blend mode '{blendMode}'")
+            #pragma warning restore format           
+        };
+
+        return blended;
+    }
+
+    private static Vector4[] Normal(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Multiply(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Screen(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Overlay(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Darken(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Lighten(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] ColorDodge(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] ColorBurn(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] HardLight(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] SoftLight(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Difference(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Exclusion(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] HslHue(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] HslSaturation(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] HslColor(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] HslLuminosity(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Addition(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Subtract(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Vector4[] Divide(Vector4[] backdrop, Vector4[] source, int opacity)
+    {
+        throw new NotImplementedException();
+    }
+}
--- source/AsepriteDotNet/Aseprite/Types/AsepriteFrame.Extensions.cs
+++ source/AsepriteDotNet/Aseprite/Types/AsepriteFrame.Extensions.cs
@@ -115,5 +115,121 @@ public static class AsepriteFrameExtensions
 
         BlendCel(backdrop, pixels, cel.Layer.BlendMode, bounds, frameWidth, cel.Opacity, cel.Layer.Opacity);
     }
+
+
+
+
+    //-----------------------------------------------------------------------------------------------------------------
+    //
+    //  Vectorized methods here
+    //
+    //-----------------------------------------------------------------------------------------------------------------
+    public static Rgba32[] FlattenFrameVectorized(this AsepriteFrame frame, bool onlyVisibleLayers = true, bool includeBackgroundLayer = false, bool includeTilemapCels = true)
+    {
+        ArgumentNullException.ThrowIfNull(frame);
+
+        Rgba32[] result = new Rgba32[frame.Size.Width * frame.Size.Height];
+        ReadOnlySpan<AsepriteCel> cels = frame.Cels;
+
+        for (int celNum = 0; celNum < cels.Length; celNum++)
+        {
+            AsepriteCel cel = cels[celNum];
+            if (cel is AsepriteLinkedCel linkedCel)
+            {
+                cel = linkedCel.Cel;
+            }
+
+            if (onlyVisibleLayers && !cel.Layer.IsVisible) { continue; }
+            if (cel.Layer.IsBackgroundLayer && !includeBackgroundLayer) { continue; }
+
+            if (cel is AsepriteImageCel imageCel)
+            {
+                BlendCelVectorized(result, imageCel.Pixels, imageCel.Layer.BlendMode, new Rectangle(imageCel.Location, imageCel.Size), frame.Size.Width, imageCel.Opacity, imageCel.Layer.Opacity);
+            }
+            else if (includeTilemapCels && cel is AsepriteTilemapCel tilemapCel)
+            {
+                BlendTilemapCelVectorized(result, tilemapCel, frame.Size.Width);
+            }
+        }
+
+        return result;
+    }
+
+    private static void BlendCelVectorized(Span<Rgba32> backdrop, ReadOnlySpan<Rgba32> source, AsepriteBlendMode blendMode, Rectangle bounds, int frameWidth, int celOpacity, int layerOpacity)
+    {
+        byte opacity = Calc.MultiplyUnsigned8Bit(celOpacity, layerOpacity);
+
+        //  Sometimes a cel can have a negative x- and/or y-coordinate location, or an x- and/or y-coordinate location
+        //  that extends outside of the bounds of the frame.  This is caused by selecting an area within Aseprite and
+        //  then moving a portion of the selected pixels outside the canvas.  We don't care about these pixels, we only
+        //  want the ones inside the frame bounds.
+        //
+        //  So we need to determine the starting and ending xy-coordinate locations within the pixels of the
+        //  cel (backdrop) that are within the frame bounds so we only process those.
+        int startX = Math.Max(0, -bounds.X);
+        int startY = Math.Max(0, -bounds.Y);
+        int endX = Math.Min(bounds.Width, frameWidth - bounds.X);
+        int endY = Math.Min(bounds.Height, backdrop.Length / frameWidth - bounds.Y);
+
+
+        //-------------------------------------------------------------------------------------------------------------
+        //
+        //  This is the original method below.  This processes cel pixel by pixel blending from backdrop (bottom) to
+        //  the layer above (source).  I'm guess for simd/vectorization you would implement some method of passing
+        //  a Vector4[] here to the AserpiteColorUtilities.Blend method?
+        //
+        //-------------------------------------------------------------------------------------------------------------
+        /*
+        for (int y = startY; y < endY; y++)
+        {
+            for (int x = startX; x < endX; x++)
+            {
+                int index = (y + bounds.Y) * frameWidth + (x + bounds.X);
+                Rgba32 b = backdrop[index];
+                Rgba32 s = source[y * bounds.Width + x]; // Index within the sliced source
+                backdrop[index] = AsepriteColorUtilities.Blend(b, s, opacity, blendMode);
+            }
+        }
+        */
+    }
+
+    private static void BlendTilemapCelVectorized(Span<Rgba32> backdrop, AsepriteTilemapCel cel, int frameWidth)
+    {
+        byte opacity = Calc.MultiplyUnsigned8Bit(cel.Opacity, cel.Layer.Opacity);
+
+        Debug.Assert(cel.Layer is AsepriteTilemapLayer);
+        AsepriteTilemapLayer aseTilemapLayer = (AsepriteTilemapLayer)cel.Layer;
+
+        AsepriteTileset tileset = aseTilemapLayer.Tileset;
+        Rectangle bounds;
+        bounds.Width = cel.Size.Width * tileset.Size.Width;
+        bounds.Height = cel.Size.Height * tileset.Size.Height;
+        bounds.X = cel.Location.X;
+        bounds.Y = cel.Location.Y;
+
+        Span<Rgba32> pixels = new Rgba32[bounds.Width * bounds.Height];
+        ReadOnlySpan<AsepriteTile> tiles = cel.Tiles;
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            AsepriteTile tile = tiles[i];
+            int column = i % cel.Size.Width;
+            int row = i / cel.Size.Width;
+            ReadOnlySpan<Rgba32> tilePixels = tileset.Pixels;
+
+            for (int j = 0; j < tilePixels.Length; j++)
+            {
+                int px = (j % tileset.Size.Width) + (column * tileset.Size.Height);
+                int py = (j / tileset.Size.Width) + (row * tileset.Size.Height);
+                int index = py * bounds.Width + px;
+                pixels[index] = tilePixels[j];
+            }
+        }
+
+        BlendCelVectorized(backdrop, pixels, cel.Layer.BlendMode, bounds, frameWidth, cel.Opacity, cel.Layer.Opacity);
+    }
+
+
+
+
 }
 

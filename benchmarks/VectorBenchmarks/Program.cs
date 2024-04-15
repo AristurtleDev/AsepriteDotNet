407c8dad6161fd1411f7086e51397babd15b3c0b
Christopher Whitley <aristurtledev@gmail.com>
2024-04-15 17:47:02 -0400

    Added vector benchmark project

---
+++ benchmarks/VectorBenchmarks/Program.cs
@@ -0,0 +1,36 @@
+﻿using AsepriteDotNet.Aseprite;
+using AsepriteDotNet.Aseprite.Types;
+using AsepriteDotNet.Common;
+using AsepriteDotNet.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+var summary = BenchmarkRunner.Run(typeof(VectorBenchmark));
+
+[SimpleJob(RuntimeMoniker.Net80)]
+[MemoryDiagnoser]
+[InProcess]
+[ReturnValueValidator]
+[GcServer(false)]
+public class VectorBenchmark
+{
+    private AsepriteFile _aseFile;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _aseFile = AsepriteFileLoader.FromFile("adventurer.aseprite");
+    }
+
+    [Benchmark(Baseline = true)]
+    public Rgba32[] FlattenFrame()
+    {
+        return _aseFile.Frames[0].FlattenFrame();
+    }
+
+    public Rgba32[] FlattenFrameVectorized()
+    {
+        return _aseFile.Frames[0].FlattenFrameVectorized();
+    }
+}

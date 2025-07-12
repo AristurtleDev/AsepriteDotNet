dfb2f397be4ef7015a26b34f0400a53006ee4a3d
Christopher Whitley <aristurtledev@gmail.com>
2025-07-11 21:33:13 -0400

    Remove cake build system, use github workflow instead

--- build/Program.cs
+++
@@ -1,7 +0,0 @@
-using AsepriteDotNet.Build;
-using Cake.Frosting;
-
-return new CakeHost()
-            .UseWorkingDirectory("../")
-            .UseContext<BuildContext>()
-            .Run(args);
--- build/Tasks/Tasks.cs
+++
@@ -1,9 +0,0 @@
-using Cake.Frosting;
-
-namespace AsepriteDotNet.Build;
-
-[TaskName("Default")]
-[IsDependentOn(typeof(BuildTask))]
-[IsDependentOn(typeof(TestTask))]
-[IsDependentOn(typeof(PackTask))]
-public sealed class DefaultTask : FrostingTask<BuildContext> { }
